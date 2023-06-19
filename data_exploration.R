4362c6ba894343b240f1265369d454b7c671a515
7AtAri <7AtAri@users.noreply.github.com>
2023-06-19 22:17:00 +0200

    data exploration updates

--- data_exploration.R
+++ data_exploration.R
@@ -5,9 +5,22 @@ wd <- getwd()
 # Print the current working directory
 print(wd)
 
-
 library(readr)
-train <- read_csv("petdata/train/train.csv")
+library(corrplot)
+library(ggplot2)
+
+# AdoptionSpeed
+# The value is determined by how quickly, if at all, a pet is adopted. 
+# The values are determined in the following way: 
+# 0 - Pet was adopted on the same day as it was listed. 
+# 1 - Pet was adopted between 1 and 7 days (1st week) after being listed. 
+# 2 - Pet was adopted between 8 and 30 days (1st month) after being listed. 
+# 3 - Pet was adopted between 31 and 90 days (2nd & 3rd month) after being listed. 
+# 4 - No adoption after 100 days of being listed. 
+# (There are no pets in this dataset that waited between 90 and 100 days).
+
+
+train <- read_csv("petdata/train/train.csv") 
 View(train)
 
 names_num <- names(which(sapply(train, is.numeric)))
@@ -19,3 +32,16 @@ length(names_num)
 pairs(train[c("Age","Breed1","Fee","Quantity","VideoAmt","PhotoAmt")])
 
 pairs(train[c("Age","Breed1","Fee","Quantity","VideoAmt","PhotoAmt", "Health","Vaccinated")])
+
+
+BreedLabels <- read_csv("petdata/PetFinder-BreedLabels.csv")
+ColorLabels <- read_csv("petdata/PetFinder-ColorLabels.csv")
+StateLabels <- read_csv("petdata/PetFinder-StateLabels.csv")
+
+?corrplot
+corr_mat=cor(train[, names_num], method="s") #create Spearman correlation matrix
+
+corrplot(corr_mat, method = "color",
+         type = "upper", order = "hclust", 
+         tl.col = "black", tl.cex=0.8) 
+
