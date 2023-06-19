5e6f2857b39c5e8b4c993047c089a417d2156c72
7AtAri <7AtAri@users.noreply.github.com>
2023-06-19 21:06:23 +0200

    update file

--- data_exploration.R
+++ data_exploration.R
@@ -6,3 +6,16 @@ wd <- getwd()
 print(wd)
 
 
+library(readr)
+train <- read_csv("petdata/train/train.csv")
+View(train)
+
+names_num <- names(which(sapply(train, is.numeric)))
+names_num
+length(names_num)
+
+#pairs(train[, names_num[1:10]])
+#pairs(train[, names_num[11:20]])
+pairs(train[c("Age","Breed1","Fee","Quantity","VideoAmt","PhotoAmt")])
+
+pairs(train[c("Age","Breed1","Fee","Quantity","VideoAmt","PhotoAmt", "Health","Vaccinated")])
